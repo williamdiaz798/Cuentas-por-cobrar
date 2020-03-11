a44520fc67cf7e0228599d05f0305f5b53511e71
williamdiaz798 <49824200+williamdiaz798@users.noreply.github.com>
2020-03-10 22:07:55 -0600

    Login menu
    es esl login y menu de operador

---
+++ CuenasPorCobrar/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace CuenasPorCobrar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.shapeContainer1 = new Microsoft.VisualBasic.PowerPacks.ShapeContainer();
+            this.lineShape2 = new Microsoft.VisualBasic.PowerPacks.LineShape();
+            this.lineShape1 = new Microsoft.VisualBasic.PowerPacks.LineShape();
+            this.TxtUsuario = new System.Windows.Forms.TextBox();
+            this.LblUsuario = new System.Windows.Forms.Label();
+            this.LblContra = new System.Windows.Forms.Label();
+            this.TxtContra = new System.Windows.Forms.TextBox();
+            this.BtnLogin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // shapeContainer1
+            // 
+            this.shapeContainer1.Location = new System.Drawing.Point(0, 0);
+            this.shapeContainer1.Margin = new System.Windows.Forms.Padding(0);
+            this.shapeContainer1.Name = "shapeContainer1";
+            this.shapeContainer1.Shapes.AddRange(new Microsoft.VisualBasic.PowerPacks.Shape[] {
+            this.lineShape2,
+            this.lineShape1});
+            this.shapeContainer1.Size = new System.Drawing.Size(431, 306);
+            this.shapeContainer1.TabIndex = 0;
+            this.shapeContainer1.TabStop = false;
+            // 
+            // lineShape2
+            // 
+            this.lineShape2.BorderColor = System.Drawing.Color.White;
+            this.lineShape2.Name = "lineShape2";
+            this.lineShape2.X1 = 200;
+            this.lineShape2.X2 = 387;
+            this.lineShape2.Y1 = 169;
+            this.lineShape2.Y2 = 169;
+            // 
+            // lineShape1
+            // 
+            this.lineShape1.BorderColor = System.Drawing.Color.White;
+            this.lineShape1.Name = "lineShape1";
+            this.lineShape1.X1 = 200;
+            this.lineShape1.X2 = 387;
+            this.lineShape1.Y1 = 87;
+            this.lineShape1.Y2 = 87;
+            // 
+            // TxtUsuario
+            // 
+            this.TxtUsuario.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(15)))), ((int)(((byte)(15)))));
+            this.TxtUsuario.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.TxtUsuario.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.TxtUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtUsuario.ForeColor = System.Drawing.Color.DimGray;
+            this.TxtUsuario.Location = new System.Drawing.Point(200, 63);
+            this.TxtUsuario.Name = "TxtUsuario";
+            this.TxtUsuario.Size = new System.Drawing.Size(213, 22);
+            this.TxtUsuario.TabIndex = 1;
+            // 
+            // LblUsuario
+            // 
+            this.LblUsuario.AutoSize = true;
+            this.LblUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblUsuario.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.LblUsuario.Location = new System.Drawing.Point(41, 63);
+            this.LblUsuario.Name = "LblUsuario";
+            this.LblUsuario.Size = new System.Drawing.Size(108, 29);
+            this.LblUsuario.TabIndex = 2;
+            this.LblUsuario.Text = "Usuario :";
+            // 
+            // LblContra
+            // 
+            this.LblContra.AutoSize = true;
+            this.LblContra.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblContra.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.LblContra.Location = new System.Drawing.Point(39, 141);
+            this.LblContra.Name = "LblContra";
+            this.LblContra.Size = new System.Drawing.Size(148, 29);
+            this.LblContra.TabIndex = 4;
+            this.LblContra.Text = "Contraseña :";
+            // 
+            // TxtContra
+            // 
+            this.TxtContra.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(15)))), ((int)(((byte)(15)))));
+            this.TxtContra.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.TxtContra.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.TxtContra.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtContra.ForeColor = System.Drawing.Color.DimGray;
+            this.TxtContra.Location = new System.Drawing.Point(200, 145);
+            this.TxtContra.Name = "TxtContra";
+            this.TxtContra.PasswordChar = '*';
+            this.TxtContra.Size = new System.Drawing.Size(213, 22);
+            this.TxtContra.TabIndex = 3;
+            // 
+            // BtnLogin
+            // 
+            this.BtnLogin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.BtnLogin.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnLogin.FlatAppearance.BorderSize = 0;
+            this.BtnLogin.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
+            this.BtnLogin.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.BtnLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnLogin.ForeColor = System.Drawing.Color.Gainsboro;
+            this.BtnLogin.Location = new System.Drawing.Point(146, 223);
+            this.BtnLogin.Name = "BtnLogin";
+            this.BtnLogin.Size = new System.Drawing.Size(150, 40);
+            this.BtnLogin.TabIndex = 5;
+            this.BtnLogin.Text = "Ingresar";
+            this.BtnLogin.UseVisualStyleBackColor = false;
+            this.BtnLogin.Click += new System.EventHandler(this.BtnLogin_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(15)))), ((int)(((byte)(15)))));
+            this.ClientSize = new System.Drawing.Size(431, 306);
+            this.Controls.Add(this.BtnLogin);
+            this.Controls.Add(this.LblContra);
+            this.Controls.Add(this.TxtContra);
+            this.Controls.Add(this.LblUsuario);
+            this.Controls.Add(this.TxtUsuario);
+            this.Controls.Add(this.shapeContainer1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Opacity = 0.95D;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Microsoft.VisualBasic.PowerPacks.ShapeContainer shapeContainer1;
+        private Microsoft.VisualBasic.PowerPacks.LineShape lineShape1;
+        private System.Windows.Forms.TextBox TxtUsuario;
+        private System.Windows.Forms.Label LblUsuario;
+        private Microsoft.VisualBasic.PowerPacks.LineShape lineShape2;
+        private System.Windows.Forms.Label LblContra;
+        private System.Windows.Forms.TextBox TxtContra;
+        private System.Windows.Forms.Button BtnLogin;
+    }
+}
+
---
+++ CuenasPorCobrar/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CuenasPorCobrar
+{
+    public partial class Form1 : Form
+    {
+        PanelAdminSistema panelAdmin = new PanelAdminSistema();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnLogin_Click(object sender, EventArgs e)
+        {
+            if (TxtUsuario.Text == "")
+            {
+                MessageBox.Show("Ingrese el Usuario");
+
+            }
+            else if (TxtContra.Text == "")
+            {
+                MessageBox.Show("Ingrese la contraseña");
+            }
+            else 
+            {
+                if (TxtUsuario.Text == "Will Diaz" && TxtContra.Text == "Asdf1234")
+                {
+                    panelAdmin.Show();
+                    this.Hide();
+                }
+                else
+                {
+                    MessageBox.Show("Datos Incorrectos");
+                }
+            }
+        }
+    }
+}
